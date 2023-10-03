e956019886d26be5b0b9e5c85ba2c36b84d0a5d2
Jess JH <lizethjh17@gmail.com>
2023-10-03 16:51:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android_gatito/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.example.android_gatito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+
+    Button B;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i= new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+}
